bf2aad2354f9bbdb3115087b9b98276b267db1b0
Juho Kesälä <elukka.elukka@gmail.com>
2018-03-20 16:54:25 +0200

    Week2 exercises: Add tests for Unicafe

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -15,6 +15,50 @@ public class MaksukorttiTest {
 
     @Test
     public void luotuKorttiOlemassa() {
-        assertTrue(kortti!=null);      
+        assertTrue(kortti != null);
     }
+
+    //kortin saldo alussa oikein
+    @Test
+    public void kortinSaldoAlussaOikein() {
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+
+    //rahan lataaminen kasvattaa saldoa oikein
+    @Test
+    public void rahanLataaminenKasvattaaSaldoaOikein() {
+        kortti.lataaRahaa(10);
+        assertEquals("saldo: 0.20", kortti.toString());
+    }
+
+    //rahan ottaminen toimii
+    @Test
+    public void rahanOttaminenToimii() {
+        assertTrue(kortti.otaRahaa(5));
+    }
+
+    //saldo vähenee oikein, jos rahaa on tarpeeksi
+    @Test
+    public void saldoVaheneeOikeinJosRahaaTarpeeksi() {
+        kortti.otaRahaa(5);
+        assertEquals("saldo: 0.05", kortti.toString());
+    }
+
+    //saldo ei muutu, jos rahaa ei ole tarpeeksi
+    @Test
+    public void saldoEiMuutuJosRahaaEiTarpeeksi() {
+        kortti.otaRahaa(12);
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    //metodi palauttaa true, jos rahat riittivät ja muuten false
+    @Test
+    public void palauttaaTrueJosRahatRiittavat() {
+        assertTrue(kortti.otaRahaa(5));
+    }
+    
+    @Test
+    public void palauttaaFalseJosRahatEiRiita() {
+        assertFalse(kortti.otaRahaa(13));
+    }
+
 }
