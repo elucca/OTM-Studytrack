0201895ddf39cac91bebcb1c0fc4cbacb1998f90
Juho Kesälä <elukka.elukka@gmail.com>
2018-03-20 15:40:43 +0200

    Week2 exercises: Add Maksukortti tests

--- laskarit/viikko2/Maksukortti/test/MaksukorttiTest.java
+++ laskarit/viikko2/Maksukortti/test/MaksukorttiTest.java
@@ -12,26 +12,78 @@ import static org.junit.Assert.*;
  */
 public class MaksukorttiTest {
 
-    public MaksukorttiTest() {
-    }
-
-    @BeforeClass
-    public static void setUpClass() {
-    }
-
-    @AfterClass
-    public static void tearDownClass() {
-    }
+    Maksukortti kortti;
 
     @Before
     public void setUp() {
-    }
-
-    @After
-    public void tearDown() {
+        kortti = new Maksukortti(10);
     }
 
     @Test
-    public void hello() {
+    public void konstruktoriAsettaaSaldonOikein() {
+        assertEquals("Kortilla on rahaa 10.0 euroa", kortti.toString());
     }
+
+    @Test
+    public void syoEdullisestiVahentaaSaldoaOikein() {
+        kortti.syoEdullisesti();
+        assertEquals("Kortilla on rahaa 7.5 euroa", kortti.toString());
+    }
+
+    @Test
+    public void syoMaukkaastiVahentaaSaldoaOikein() {
+        kortti.syoMaukkaasti();
+        assertEquals("Kortilla on rahaa 6.0 euroa", kortti.toString());
+    }
+
+    @Test
+    public void syoEdullisestiEiVieSaldoaNegatiiviseksi() {
+        kortti.syoMaukkaasti();
+        kortti.syoMaukkaasti();
+        kortti.syoEdullisesti();
+        assertEquals("Kortilla on rahaa 2.0 euroa", kortti.toString());
+    }
+
+    public void syoMaukkaastiEiVieSaldoaNegatiiviseksi() {
+        kortti.syoEdullisesti();
+        kortti.syoEdullisesti();
+        kortti.syoEdullisesti();
+        kortti.syoMaukkaasti();
+        assertEquals("Kortilla on rahaa 0.0 euroa", kortti.toString());
+    }
+
+    @Test
+    public void kortilleVoiLadataRahaa() {
+        kortti.lataaRahaa(25);
+        assertEquals("Kortilla on rahaa 35.0 euroa", kortti.toString());
+    }
+
+    @Test
+    public void kortinSaldoEiYlitaMaksimiarvoa() {
+        kortti.lataaRahaa(200);
+        assertEquals("Kortilla on rahaa 150.0 euroa", kortti.toString());
+    }
+
+    @Test
+    public void negatiivisenSummanLataaminenEiMuutaKortinSaldoa() {
+        kortti.lataaRahaa(-10);
+        assertEquals("Kortilla on rahaa 10.0 euroa", kortti.toString());
+    }
+
+    @Test
+    public void pystyyOstamaanEdullisenLounaanJosJuuriTarpeeksiRahaa() {
+        //Ylimääräistä että setupissa luodaan maksukortti tämän testin suhteen
+        //kun siihen tarvitaan eri saldo. Mikä on paras tapa?
+        Maksukortti testiKortti = new Maksukortti(2.5);
+        testiKortti.syoEdullisesti();
+        assertEquals("Kortilla on rahaa 0.0 euroa", testiKortti.toString());
+    }
+
+    @Test
+    public void pystyyOstamaanMaukkaanLounaanJosJuuriTarpeeksiRahaa() {
+        Maksukortti testiKortti = new Maksukortti(4.0);
+        testiKortti.syoMaukkaasti();
+        assertEquals("Kortilla on rahaa 0.0 euroa", testiKortti.toString());
+    }
+
 }
